5b0be33450ab24bfb5e180fb150c59b561314de3
Ase Fagerbakke <ase.fagerbakke@student.uib.no>
2024-09-17 09:12:01 +0200

    Prøve Ai, like mange

--- src/main/java/no/uib/inf102/wordle/controller/AI/MyStrategy.java
+++ src/main/java/no/uib/inf102/wordle/controller/AI/MyStrategy.java
@@ -1,29 +1,43 @@
 package no.uib.inf102.wordle.controller.AI;
 
+import no.uib.inf102.wordle.model.Dictionary;
 import no.uib.inf102.wordle.model.word.WordleWord;
 import no.uib.inf102.wordle.model.word.WordleWordList;
-;
 
+;
 
 public class MyStrategy implements IStrategy {
 
+    private Dictionary dictionary;
     private WordleWordList guesses;
-    
+
+    public MyStrategy(Dictionary dictionary) {
+        this.dictionary = dictionary;
+        reset();
+    }
+
+//String firstUniqueWord = guesses.findFirstAllDifferent();
 //få færre gjetninger. 
     @Override
     public String makeGuess(WordleWord feedback) {
+        if (feedback == null) {
+            // Velg det første ordet med alle forskjellige bokstaver
+            String firstUniqueWord = guesses.findFirstOptimized();
+            if (firstUniqueWord != null) {
+                return firstUniqueWord;
+            }
+        }
+
+        // Hvis det ikke er det første gjettet, eller ingen unike ord ble funnet, bruk frekvensmetoden
         if (feedback != null) {
-            guesses.eliminateWords(feedback);    
-        } 
+            guesses.eliminateWords(feedback);
+        }
+
         return guesses.bestWord();
     }
 
-
     @Override
     public void reset() {
-        // TODO: Implement me :)
-        throw new UnsupportedOperationException("Unimplemented method 'reset'");
+        guesses = new WordleWordList(dictionary);
     }
-
-    
 }
--- src/main/java/no/uib/inf102/wordle/model/word/WordleWordList.java
+++ src/main/java/no/uib/inf102/wordle/model/word/WordleWordList.java
@@ -16,161 +16,189 @@ import no.uib.inf102.wordle.model.Dictionary;
  */
 public class WordleWordList {
 
-	/**
-	 * All words in the game. These words can be used as guesses.
-	 */
-	private Dictionary allWords;
+    /**
+     * All words in the game. These words can be used as guesses.
+     */
+    private Dictionary allWords;
 
-	/**
-	 * A subset of <code>allWords</code>. <br>
-	 * </br>
-	 * These words can be the answer to a wordle game.
-	 */
-	private List<String> possibleAnswers;
+    /**
+     * A subset of <code>allWords</code>. <br>
+     * </br>
+     * These words can be the answer to a wordle game.
+     */
+    private List<String> possibleAnswers;
 
-	/**
-	 * Create a WordleWordList that uses the full words and limited answers of the
-	 * GetWords class.
-	 */
-	public WordleWordList(Dictionary dictionary) {
-		this.allWords = dictionary;
-		this.possibleAnswers = new ArrayList<>(dictionary.getAnswerWordsList());
-	}
+    /**
+     * Create a WordleWordList that uses the full words and limited answers of
+     * the GetWords class.
+     */
+    public WordleWordList(Dictionary dictionary) {
+        this.allWords = dictionary;
+        this.possibleAnswers = new ArrayList<>(dictionary.getAnswerWordsList());
+    }
 
-	/**
-	 * Get the list of all guessing words.
-	 * 
-	 * @return all words
-	 */
-	public Dictionary getAllWords() {
-		return allWords;
-	}
+    /**
+     * Get the list of all guessing words.
+     *
+     * @return all words
+     */
+    public Dictionary getAllWords() {
+        return allWords;
+    }
 
-	/**
-	 * Returns the list of possible answers.
-	 * 
-	 * @return
-	 */
-	public List<String> possibleAnswers() {
-		return Collections.unmodifiableList(possibleAnswers);
-	}
+    /**
+     * Returns the list of possible answers.
+     *
+     * @return
+     */
+    public List<String> possibleAnswers() {
+        return Collections.unmodifiableList(possibleAnswers);
+    }
 
-	/**
-	 * Eliminates words from the possible answers list using the given
-	 * <code>feedback</code>
-	 * 
-	 * @param feedback the feedback to eliminate words with. 
-	 */
-	public void eliminateWords(WordleWord feedback) {
-		List<String> newPossibleAnswer=new ArrayList<>(size());
+    /**
+     * Eliminates words from the possible answers list using the given
+     * <code>feedback</code>
+     *
+     * @param feedback the feedback to eliminate words with.
+     */
+    public void eliminateWords(WordleWord feedback) {
+        List<String> newPossibleAnswer = new ArrayList<>(size());
 
-		for (String word : possibleAnswers){ 
-			if (WordleWord.isPossibleWord(word,feedback)){
-				newPossibleAnswer.add(word);
-			}
-			possibleAnswers=newPossibleAnswer; //update the possible answers
-		}
-	}
-
-	/**
-	 * Returns the amount of possible answers in this WordleWordList
-	 * 
-	 * @return size of
-	 */
-	public int size() {
-		return possibleAnswers.size();
-	}
-
-	/**
-	 * Removes the given <code>answer</code> from the list of possible answers.
-	 * 
-	 * @param answer
-	 */
-	public void remove(String answer) {
-		possibleAnswers.remove(answer);
-	}
-
-	/**
-	 * Returns the word length in the list of valid guesses.
-	 * 
-	 * @return
-	 */
-	public int wordLength() {
-		return allWords.WORD_LENGTH;
-	}
-
-/**
- * Returns the best possible word, given the answer.
- */
-public String bestWord() {
-    List<HashMap<Character, Integer>> bestFrequency = bestFrequency(possibleAnswers);
-    String bestWord = "";
-    int maxCount = -1;
-
-    // Find the word with the highest frequency of characters
-    for (String word : possibleAnswers) {
-        int tempCount = 0;
-
-        for (int i = 0; i < wordLength(); i++) {
-            char c = word.charAt(i);
-            if (bestFrequency.get(i).containsKey(c)) {
-				tempCount += bestFrequency.get(i).get(c);
-			} else {
-				tempCount += 0;
-			}
-
-        }
-
-        // Update the best word if this word has a higher score
-        if (tempCount > maxCount) {
-            maxCount = tempCount;
-            bestWord = word;
+        for (String word : possibleAnswers) {
+            if (WordleWord.isPossibleWord(word, feedback)) {
+                newPossibleAnswer.add(word);
+            }
+            possibleAnswers = newPossibleAnswer; //update the possible answers
         }
     }
 
-    return bestWord;
-}
+    /**
+     * Returns the amount of possible answers in this WordleWordList
+     *
+     * @return size of
+     */
+    public int size() {
+        return possibleAnswers.size();
+    }
 
-	 //count the number of possible words for each character
-	 // holder oversikt over hvor mange ganger hver bokstav forekommer i hver posisjon
-	 private List<HashMap<Character, Integer>> bestFrequency(List<String> possibleAnswers){
-		List<HashMap<Character, Integer>> bestFrequency= new ArrayList<>();
+    /**
+     * Removes the given <code>answer</code> from the list of possible answers.
+     *
+     * @param answer
+     */
+    public void remove(String answer) {
+        possibleAnswers.remove(answer);
+    }
 
-		for (int i = 0; i < wordLength(); i++) {
-			bestFrequency.add(new HashMap<>());
-		}
+    /**
+     * Returns the word length in the list of valid guesses.
+     *
+     * @return
+     */
+    public int wordLength() {
+        return allWords.WORD_LENGTH;
+    }
 
-		for(String word: possibleAnswers){
+    /**
+     * Returns the best possible word, given the answer.
+     */
+    public String bestWord() {
+        List<HashMap<Character, Integer>> bestFrequency = bestFrequency(possibleAnswers);
+        String bestWord = "";
+        int maxCount = -1;
+
+        // Find the word with the highest frequency of characters
+        for (String word : possibleAnswers) {
+            int tempCount = 0;
+
+            for (int i = 0; i < wordLength(); i++) {
+                char c = word.charAt(i);
+                if (bestFrequency.get(i).containsKey(c)) {
+                    tempCount += bestFrequency.get(i).get(c);
+                } else {
+                    tempCount += 0;
+                }
+
+            }
+
+            // Update the best word if this word has a higher score
+            if (tempCount > maxCount) {
+                maxCount = tempCount;
+                bestWord = word;
+            }
+        }
+
+        return bestWord;
+    }
+
+    //count the number of possible words for each character
+    // holder oversikt over hvor mange ganger hver bokstav forekommer i hver posisjon
+    private List<HashMap<Character, Integer>> bestFrequency(List<String> possibleAnswers) {
+        List<HashMap<Character, Integer>> bestFrequency = new ArrayList<>();
+
+        for (int i = 0; i < wordLength(); i++) {
+            bestFrequency.add(new HashMap<>());
+        }
+
+        for (String word : possibleAnswers) {
+            for (int i = 0; i < wordLength(); i++) {
+                char c = word.charAt(i);
+                if (bestFrequency.get(i).containsKey(c)) {
+                    bestFrequency.get(i).put(c, bestFrequency.get(i).get(c) + 1);
+                } else {
+                    bestFrequency.get(i).put(c, 1);
+                }
+            }
+        }
+
+        return bestFrequency;
+    }
+
+    //koder til min Ai
+    //return true if it's all different characters in the word
+	//*
+	
+    //public boolean allDifferent(String word) {
+        //Set<Character> set = new HashSet<>();
+        //for (int i = 0; i < word.length(); i++) {
+            //char c = word.charAt(i); //O(1)
+            //if (set.contains(c)) { //O(1)
+                //return false;
+            //} else {
+                //set.add(c); //O(1)
+            //}
+       // }
+
+        //return true;
+    //}
+	
+    // Finn det første ordet med alle forskjellige bokstaver
+
+	public String findFirstOptimized() {
+		List<HashMap<Character, Integer>> bestFrequency = bestFrequency(possibleAnswers);
+		String bestWord = "";
+		int maxScore = -1;
+	
+		for (String word : possibleAnswers) {
+			int tempScore = 0;
+			Set<Character> uniqueChars = new HashSet<>();
+	
 			for (int i = 0; i < wordLength(); i++) {
 				char c = word.charAt(i);
-				if (bestFrequency.get(i).containsKey(c)){
-					bestFrequency.get(i).put(c, bestFrequency.get(i).get(c)+1);
-				}else{
-					bestFrequency.get(i).put(c, 1);
-				}
+				tempScore += bestFrequency.get(i).getOrDefault(c, 0);
+				uniqueChars.add(c);
 			}
-		}
-
-		return bestFrequency;
-	 }
-
-	 //return true if it's all different characters in the word
-	 public boolean allDifferent(String word){
-		Set<Character> set = new HashSet<>();
-		for (int i = 0; i < word.length(); i++) {
-			char c = word.charAt(i); //O(1)
-			if (set.contains(c)) { //O(1)
-				return false;
-			} else {
-				set.add(c); //O(1)
-			}
-		}
-
-		return true;
-	}
 	
-
-
+			// Legg til poeng for antall unike bokstaver
+			tempScore += uniqueChars.size();
+	
+			if (tempScore > maxScore) {
+				maxScore = tempScore;
+				bestWord = word;
+			}
+		}
+	
+		return bestWord;
 	}
-	 
 
+}
